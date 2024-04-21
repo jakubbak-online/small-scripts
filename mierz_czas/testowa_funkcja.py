223b1ea64a4dfe0e5ca041e87c2ecb6e92200f39
Jakub Bąk <kontakt@jakubbak.online>
2024-04-21 20:46:51 +0200

    Reversed change to package

--- mierz_czas/testowa_funkcja.py
+++ mierz_czas/testowa_funkcja.py
@@ -1,9 +1,13 @@
 from mierz_czas import mierz_czas
+from random import randint
+
 
 @mierz_czas
-def testowa_funkcja(iter, divisor):
-    for _ in range(0, iter):
-        if _ % divisor == 0:
-            print(_)
+def testowa_funkcja(iter_num, divisor):
+    for _ in range(0, iter_num):
+        rand_num = randint(1, 100000)
+        if rand_num % divisor == 0:
+            print(f"{_}. {rand_num}")
 
-testowa_funkcja(200000000, 2543)
+
+testowa_funkcja(20000000, 47833)
